7c93b45fa243046d43002eba6fbf98021d7e8c52
georgifotev1 <georgifotev12@gmail.com>
2022-12-03 14:53:23 +0200

    Created the base of the header and footer of the project

--- client/Car-Market/src/app/app.module.ts
+++ client/Car-Market/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, CoreModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, CoreModule, NoopAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- client/Car-Market/src/app/core/core.module.ts
+++ client/Car-Market/src/app/core/core.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent, FooterComponent],
   imports: [CommonModule],
-  exports: [],
+  exports: [HeaderComponent, FooterComponent],
 })
 export class CoreModule {}
