3391768588f6370b9d8e2310e6be6650dd99f421
georgifotev1 <georgifotev12@gmail.com>
2022-12-03 08:43:57 +0200

    Adding the base of the server

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const cors = require("./middlewares/cors");
+const trimBody = require("./middlewares/trimBody");
+const session = require("./middlewares/session");
+const authController = require("./controllers/authController");
+
+const connectionString = "mongodb://localhost:27017/car-market";
+
+async function start() {
+  await mongoose.connect(connectionString);
+  console.log("Database connected");
+
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+  app.use(trimBody());
+  app.use(session());
+
+  app.get("/", (req, res) => {
+    res.json({ message: "REST service operational" });
+  });
+
+  app.use("/api", authController);
+
+  app.listen(3030, () => console.log("REST service started"));
+}
+
+start();
