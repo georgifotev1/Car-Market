975c38eafa655fc50c8b7dbba828057076be301e
georgifotev1 <georgifotev12@gmail.com>
2022-12-04 10:51:15 +0200

    Added a Not Found Page

--- client/Car-Market/src/app/app-routing.module.ts
+++ client/Car-Market/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
 import { MainComponent } from './main/main.component';
 
 const routes: Routes = [
@@ -7,6 +8,14 @@ const routes: Routes = [
     path: '',
     pathMatch: 'full',
     component: MainComponent,
+  },
+  {
+    path: 'not-found',
+    component: PageNotFoundComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '/not-found',
   },
 ];
 
