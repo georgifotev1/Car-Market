aa68a6a3ba432620a8e0f599713125186bdcb0e2
georgifotev1 <georgifotev12@gmail.com>
2022-12-10 11:28:09 +0200

    Created the register form

--- client/Car-Market/src/app/auth/auth.module.ts
+++ client/Car-Market/src/app/auth/auth.module.ts
@@ -4,10 +4,12 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LogoutComponent } from './logout/logout.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent, LogoutComponent],
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [CommonModule, AuthRoutingModule, SharedModule, ReactiveFormsModule],
   exports: [LoginComponent, RegisterComponent, LogoutComponent],
 })
 export class AuthModule {}
--- client/Car-Market/src/app/auth/register/register.component.ts
+++ client/Car-Market/src/app/auth/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { sameValueGroupValidator } from 'src/app/shared/validators';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
-  constructor() {}
+  form = this.fb.group({
+    username: ['', [Validators.required, Validators.minLength(3)]],
+    email: ['', [Validators.required, Validators.email]],
+    pass: this.fb.group(
+      {
+        password: ['', Validators.minLength(5)],
+        rePass: [],
+      },
+      {
+        validators: [sameValueGroupValidator('password', 'rePass')],
+      }
+    ),
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  registerHandler() {
+    console.log('click');
+  }
 }
